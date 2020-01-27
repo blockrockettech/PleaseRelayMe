3a717264d299607940f10afd6a78bdac37162a4c
Vincent de Almeida <vince0656@googlemail.com>
2020-01-27 13:40:19 +0000

    Integrated kyber into the relay hub

--- smart-contracts/contracts/relay/rDAIRelayHub.sol
+++ smart-contracts/contracts/relay/rDAIRelayHub.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./RelayHub.sol";
 import "../rDAI/IRToken.sol";
-import "../liquidity/UniswapExchangeInterface.sol";
+import "../liquidity/KyberNetworkInterface.sol";
 
 contract rDAIRelayHub is RelayHub {
     using SafeMath for uint256;
@@ -12,7 +12,7 @@ contract rDAIRelayHub is RelayHub {
     address ETH_PROXY = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address KYBER_NETWORK_PROXY_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-    function refuelFor(address target, IRToken rDai, IERC20 dai, UniswapExchangeInterface uniswap) external {
+    function refuelFor(address target, IRToken rDai, IERC20 dai, KyberNetworkInterface kyber) external {
         // 1. Claim earned rDAI interest
         address self = address(this);
         bool interestClaimSuccess = rDai.payInterest(self);
@@ -23,8 +23,21 @@ contract rDAIRelayHub is RelayHub {
         require(redeemSuccess, "Failed to redeem underlying DAI from interest paid");
 
         // 3. TODO: Swap DAI for ETH
-        dai.approve(KYBER_NETWORK_PROXY_ADDRESS, dai.balanceOf(self));
-        uint256 ethReceivedFromDaiSale = 0;
+        uint256 daiBalance = dai.balanceOf(self);
+        dai.approve(KYBER_NETWORK_PROXY_ADDRESS, daiBalance);
+
+        (uint expectedDaiEthExhangeRate, ) = kyber.getExpectedRate(dai, IERC20(ETH_PROXY), daiBalance);
+        uint256 ethReceivedFromDaiSale = kyber.tradeWithHint(
+            self,
+            dai,
+            daiBalance,
+            IERC20(ETH_PROXY),
+            self,
+            2**256 - 1,
+            expectedDaiEthExhangeRate,
+            address(0),
+            abi.encodePacked(uint256(0))
+        );
 
         // 4. Update target contract's relay balance
         balances[target] = balances[target].add(ethReceivedFromDaiSale);
