b999150c9d4d9d9e661a99c7188035938eda16a1
Vincent de Almeida <vince0656@googlemail.com>
2020-01-27 11:55:48 +0000

    Retrieving interest and claiming underlying asset

--- smart-contracts/contracts/relay/rDAIRelayHub.sol
+++ smart-contracts/contracts/relay/rDAIRelayHub.sol
@@ -7,7 +7,13 @@ import "../liquidity/UniswapExchangeInterface.sol";
 contract rDAIRelayHub is RelayHub {
     function refuelFor(address target, IRToken rDai, UniswapExchangeInterface uniswap) external {
         // 1. Claim earned rDAI interest
+        bool interestClaimSuccess = rDai.payInterest(address(this));
+        require(interestClaimSuccess, "Failed to claim interest earned for target");
+
         // 2. Redeem underlying DAI from rDAI contract
+        bool redeemSuccess = rDai.redeemAll();
+        require(redeemSuccess, "Failed to redeem underlying DAI from interest paid");
+
         // 3. Swap DAI for ETH
         // 4. Update target contract's relay balance
     }
