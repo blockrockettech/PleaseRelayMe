6a28d6a18f61ee0dc818d89661f645ef6ea3cf71
Vincent de Almeida <vince0656@googlemail.com>
2020-01-31 16:15:16 +0000

    updated migration

--- smart-contracts/migrations/2_rdai_relay_hub.js
+++ smart-contracts/migrations/2_rdai_relay_hub.js
@@ -1,7 +1,15 @@
 const rDAIRelayHub = artifacts.require('rDAIRelayHub');
+const MockLiquidityProvider = artifacts.require('MockLiquidityProvider');
 
 module.exports = async function (deployer, network, accounts) {
-    console.log(`Deploying rDAI Relay Hub on [${network}] using [${accounts[0]}]`);
-    const hub = deployer.deploy(rDAIRelayHub, {from: accounts[0]});
+    console.log(`Deploying rDAI Relay Hub and mock liqidity provider on [${network}] using [${accounts[0]}]`);
+
+    // Todo: needs swapping out for real liquidity provider
+    const liquidityProvider = await deployer.deploy(MockLiquidityProvider, {from: accounts[0]});
+
+    // Todo: replace kovan deployment addresses below
+    const DAI = '0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa';
+    const rDAI = '0x462303f77a3f17Dbd95eb7bab412FE4937F9B9CB';
+    const hub = await deployer.deploy(rDAIRelayHub, DAI, rDAI, liquidityProvider.address, {from: accounts[0]});
     console.log(`Deployed at ${hub.address}`);
 };
